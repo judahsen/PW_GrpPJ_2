52a440e991bcbac73fe0cc1b4f0ecef35b511c83
Manny <147103853+N0l0z@users.noreply.github.com>
2023-12-01 19:38:30 -0800

    Update OOPrkGrg.py
    added class and lists

--- OOPrkGrg.py
+++ OOPrkGrg.py
@@ -1,1 +1,52 @@
-print("Hi")
+"""
+#Your parking gargage class should have the following methods:
+
+#- takeTicket
+#- This should decrease the amount of tickets available by 1
+#- This should decrease the amount of parkingSpaces available by 1
+
+#- payForParking
+
+# - Display an input that waits for an amount from the user and store it in a variable
+# - If the payment variable is not empty then (meaning the ticket has been paid) 
+-> display a message to the user that their ticket has been paid and they have 15mins to leave
+- This should update the "currentTicket" dictionary key "paid" to True
+-leaveGarage
+
+- If the ticket has been paid, display a message of "Thank You, have a nice day"
+- If the ticket has not been paid, display an input prompt for payment
+- Once paid, display message "Thank you, have a nice day!"
+
+- Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
+- Update tickets list to increase by 1 (meaning add to the tickets list)
+
+You will need a few attributes as well:
+- tickets -> list
+- parkingSpaces -> list
+- currentTicket -> dictionary
+"""
+#parking_spaces = [1, 2, 3, 4, 5]
+#tickets = [1, 2, 3, 4, 5,]
+
+        
+class Parking_Garage():
+#display parking spaces show(parking_spaces) s="(input{f'show'} choose a space:  "):
+    
+    def __init__(self, tickets, parking_spaces):
+        
+
+        self.current_ticket = {}
+        self.tickets = [1, 2, 3, 4, 5,]
+        self.parking_spaces = parking_spaces
+    def take_ticket(self):
+    
+s = input(f"{parking_spaces} Choose a space!")
+
+
+
+
+
+
+
+
+
